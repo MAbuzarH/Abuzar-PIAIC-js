4b07d3c9e921bb33e66d17f333935a1205ef3c7d
MAbuzarH <103282484+MAbuzarH@users.noreply.github.com>
2022-10-04 09:24:11 -0700

    Update Q16.js

---
+++ piaic js project/Q16.js
@@ -0,0 +1,5 @@
+let gest = ["ali", "qasam", "bakar"];
+for (let i = 0; i < gest.length; i++) {
+  console.log(gest[i] + " i am inviting you on dinner");
+}
+console.log("I found a bigger dinner table.");
